57a26cda3e73e07f87063ec2f2ffda4bae2b2b80
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-02-28 13:20:22 -0600

    coffee css added

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -54,6 +54,31 @@ const Auth = () => {
           Need to {register ? "Login" : "Sign Up"}?
         </button>
       </Card>
+
+      <section>
+        <div className="coffee-animation">
+          <div class="container">
+            <div class="coffee-header">
+              <div class="coffee-header__buttons coffee-header__button-one"></div>
+              <div class="coffee-header__buttons coffee-header__button-two"></div>
+              <div class="coffee-header__display"></div>
+              <div class="coffee-header__details"></div>
+            </div>
+            <div class="coffee-medium">
+              <div class="coffe-medium__exit"></div>
+              <div class="coffee-medium__arm"></div>
+              <div class="coffee-medium__liquid"></div>
+              <div class="coffee-medium__smoke coffee-medium__smoke-one"></div>
+              <div class="coffee-medium__smoke coffee-medium__smoke-two"></div>
+              <div class="coffee-medium__smoke coffee-medium__smoke-three"></div>
+              <div class="coffee-medium__smoke coffee-medium__smoke-for"></div>
+              <div class="coffee-medium__cup"></div>
+            </div>
+            <div class="coffee-footer"></div>
+          </div>
+        </div>
+        <h3>Grab a cup-o-coffee, this could take a while...</h3>
+      </section>
     </main>
   );
 };
