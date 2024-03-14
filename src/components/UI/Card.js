18ebb8f3347167c5dc10ea5c5cbaab65c1327a98
Jennifer Arias <jenniferarias414@gmail.com>
2024-03-14 08:25:54 -0500

    adjusted metadata

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css' //card container (wrapper) component
-
-function Card(props){
-    const classes = 'card ' + props.className
-    return <div className={classes}>{props.children}</div>
-}
-
-export default Card;
